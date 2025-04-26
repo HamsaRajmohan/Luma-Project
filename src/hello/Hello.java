38eab3eff71e5eea882fe59c2d4848f3d1b5d192
HamsaRajmohan <hamsarakesh2805@gmail.com>
2025-04-26 12:29:40 +0530

    Changes done

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,6 +2,12 @@ package hello;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("Hamsa");
+	//System.out.println("Hamsa");
+	Hello obj=new Hello();
+	obj.namePerson();
+}
+
+public void namePerson() {
+	System.out.println("Hamsa Rajmohan name");
 }
 }
