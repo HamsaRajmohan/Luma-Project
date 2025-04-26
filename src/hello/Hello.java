bb94411149b68d5570c547d373caae7781848028
HamsaRajmohan <hamsarakesh2805@gmail.com>
2025-04-26 12:50:28 +0530

    Renamed

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,6 +2,12 @@ package hello;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("Hamsa");
+	//System.out.println("Hamsa");
+Hello hello=new Hello();
+hello.rename();
+}
+
+public void rename() {
+	System.out.println("Rename as Sheela");
 }
 }
